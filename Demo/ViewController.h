fb81590610e50396975b9b1ad9b5c5a92cb61c86
chkkassd <sangdeliang@haoweibin.local>
2013-11-21 15:52:43 +0800

    Initial Commit

---
+++ Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by shangdejigou on 13-11-21.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by shangdejigou on 13-11-21.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
